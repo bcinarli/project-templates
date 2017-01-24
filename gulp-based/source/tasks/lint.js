16aa6d9da387d3c63353d32b3609baa2bd819452
Bilal Çınarlı <bcinarli@gmail.com>
2017-01-24 13:18:25 +0300

    [Gulp Base] Linting tasks updated

--- gulp-based/source/tasks/lint.js
+++ gulp-based/source/tasks/lint.js
@@ -5,19 +5,19 @@
 var lint = function(gulp, options, plugins) {
     gulp.task('lint:scss', function() {
         return gulp.src(options.config.paths.scss + '**\/*.scss')
-            .pipe(plugins.sassLint())
-            .pipe(plugins.sassLint.format())
-            .pipe(plugins.sassLint.failOnError())
-            .pipe(plugins.notify({message: 'Scss Lint completed', onLast: true}));
+            .pipe(plugins.stylelint({
+                reporters: [
+                    {formatter: 'string', console: true}
+                ]
+            }))
+            .pipe(plugins.notify({message: 'Style Linting completed', onLast: true}));
     });
 
     gulp.task('lint:js', function() {
         return gulp.src(options.config.paths.js + '**\/*.js')
-            .pipe(plugins.jshint()).on('error', plugins.notify.onError('Error: <%= error.message %>'))
-            .pipe(plugins.jshint.reporter('default', {
-                fail: true
-            }))
-            .pipe(plugins.notify({message: 'JSHint completed', onLast: true}));
+            .pipe(plugins.eslint())
+            .pipe(plugins.eslint.format())
+            .pipe(plugins.notify({message: 'ESLint completed', onLast: true}));
     });
 };
 
